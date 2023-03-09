ab6e06ec479ba5cad27d8c298894ad6bd91c0169
jitu2404 <jatin.ahluwalia.5@gmail.com>
2023-03-10 00:26:00 +0530

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = document.querySelector(".search")
+const searchBar = document.querySelector(".search-bar")
+
+search.addEventListener("focus", e => {
+    searchBar.style.border = "1px solid white"
+})
